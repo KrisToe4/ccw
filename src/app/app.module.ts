c239d1ff49530eed3dbe7fd9cc4ef62acb965cd9
Kris Wilson <kristofor.wilson@gmail.com>
2017-12-07 23:42:39 -0800

    Added basic header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MaterialsListModule } from '@modules/materials-list.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@modules/material.module';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { LandingComponent } from '@components/landing/landing.component'
 import { NotFoundComponent } from '@components/not-found/not-found.component';
+import { HeaderComponent } from '@components/header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     LandingComponent,
     NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    MaterialsListModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/modules/material.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {MatButtonModule,
+        MatCardModule,
+        MatIconModule, 
+        MatToolbarModule} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule, 
+  ],
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule, 
+  ],
+})
+export class MaterialModule { }
